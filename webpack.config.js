a2283dee147e32ccb2cca9731b991002f41e974f
TraBee <tra8brown@gmail.com>
2021-09-13 16:39:53 -0600

    jquery plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+const webpack = require("webpack")
 const path = require("path");
 
 //webpack 3 properties: entry, output & mode
 module.exports = {
     entry: "./assets/js/script.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: path.join(__dirname + "/dist"),
+        filename: "main.bundle.js"
     },
-    mode: 'development'
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
+    mode: "development"
 };
